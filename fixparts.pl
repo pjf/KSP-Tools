50d34bd60b266e43aec59648f1997ab8b90dc599
Paul Fenwick <pjf@perltraining.com.au>
2014-07-11 17:17:24 -0700

    Moar parts removed.

--- fixparts.pl
+++ fixparts.pl
@@ -12,36 +12,61 @@ use File::Path qw(remove_tree);
 my %unloved = (
 
     # Fairings (because ProceduralFairings rock)
-    fairings => [ qw(
-        FASA/Apollo/FASA_Apollo_Fairings
-        FASA/Gemini2/FASA_Fairings_Plate_2m
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone1m.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone1m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone2mHalf.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone3M.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone3m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone625m.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone625m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNoseconeHalf2m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall05m.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall05m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall1M.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall1m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2m.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2mS.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2mS.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall3M.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall3m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone1m.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone1m.mu
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone2m.cfg
-        FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone2m.mu
-    ) ],
+    fairings => [
+        'FASA/Apollo/FASA_Apollo_Fairings',
+        'FASA/Gemini2/FASA_Fairings_Plate_2m',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone1m.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone1m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone2mHalf.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone3M.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone3m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone625m.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNosecone625m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingNoseconeHalf2m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall05m.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall05m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall1M.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall1m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2m.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2mS.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall2mS.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall3M.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWall3m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone1m.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone1m.mu',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone2m.cfg',
+        'FASA/Gemini2/FASA_Gemini_LR91_Pack/FairingWallCone2m.mu',
+        'AIES_Aerospace/Structure/AIES t1000v4',
+        'AIES_Aerospace/Structure/AIES t1500v4',
+        'AIES_Aerospace/Structure/AIES t2000v4',
+        'AIES_Aerospace/Structure/AIES t3000r20c',
+        'AIES_Aerospace/Structure/AIES top100 a1ur5',
+        'AIES_Aerospace/Structure/AIES top100 a1ur5b',
+        'AIES_Aerospace/Structure/AIES top500 a1ur5',
+    ],
 
     # Nosecones (ProceduralParts is awesome)
-    nosecones => [
-        'NovaPunch2/Parts/NoseCone',
+    nosecones => [qw(
+        NovaPunch2/Parts/NoseCone
+        NovaPunch2/NP_SRB_2_5m_AdvSRB/0_625mASRBNose.cfg
+        NovaPunch2/NP_SRB_2_5m_AdvSRB/1_25mASRBNose.cfg
+        NovaPunch2/NP_SRB_2_5m_AdvSRB/2_5mASRBNose.cfg
+        NovaPunch2/NP_SRB_2_5m_AdvSRB/TD_JupiterSRBNose.png
+        NovaPunch2/NP_SRB_2_5m_AdvSRB/TD_JupiterSRBNoseNRM.png
+        NovaPunch2/NP_SRB_2_5m_AdvSRB/nose.mu
+
+        NovaPunch2/NP_SRB_AdvBLite/Srb025mNose.cfg
+        NovaPunch2/NP_SRB_AdvBLite/Srb0625mNose.cfg
+        NovaPunch2/NP_SRB_AdvBLite/Srb125mNose.cfg
+        NovaPunch2/NP_SRB_AdvBLite/nose.mu
+        NovaPunch2/NP_SRB_AdvBLite/nosefairing.png
+        NovaPunch2/NP_SRB_AdvBLite/nosefairingNRM.png
+
+        AIES_Aerospace/Aero/AIEScone0.5Ra
+        AIES_Aerospace/Aero/AIESconenoseram
+
+        ),
 
         # These are all warheads...
         'RftS/Parts/KWNoseconeBlack',
@@ -50,6 +75,7 @@ my %unloved = (
         'RftS/Mk3RV',
         'RftS/Mk4RV.cfg',
         'FASA/ICBM/FASA_ICBM_Nosecone',
+
 
         # I'm leaving the Gemini nosecones in, because they don't
         # suck.
